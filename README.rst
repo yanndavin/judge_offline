6dc9c307bc9ec5db12c3e65fb9187e11024c23db
yann davin <yann.davin@gmail.com>
2017-05-14 12:35:47 +0200

    -fix missing README for python setup -fix wrong project name in setup.py

---
+++ README.rst
@@ -0,0 +1,4 @@
+Judge Offline for Python3
+===========================================================
+
+Simple module to help testing programming contest exercises.
--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='jujuje',
+    name='judge_offline',
     version='0.1.0',
     description='Provides personal judge similar hackrank or judge online',
     long_description=readme,
     author='Yann Davin',
     author_email='yann.davin@gmail.com',
-    url='https://github.com/yann.davin/jujuge',
+    url='https://github.com/yanndavin/judge_offline',
     license=license,
     packages=find_packages(exclude=('samples', 'docs'))
 )
